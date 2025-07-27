12904c51b4b7cb1cecd756d01967dc0dc4d79039
Alejandro R <arc19881@gmail.com>
2025-07-28 00:10:19 +0200

    login

--- facturacion/settings.py
+++ facturacion/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Carga el archivo .env automáticamente
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
