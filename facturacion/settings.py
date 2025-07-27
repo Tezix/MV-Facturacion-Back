c1494e4d54df054cde86f4bebd1e2d078a934131
Alejandro R <arc19881@gmail.com>
2025-07-27 22:32:51 +0200

    test

--- facturacion/settings.py
+++ facturacion/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3a_pj1c^uhe(-zh^5r)t9w%@wio!(ser_u3z(n%q!t)wd!5zb1'
+SECRET_KEY = os.environ.get('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mvfacturacion.netlify.app']
 
